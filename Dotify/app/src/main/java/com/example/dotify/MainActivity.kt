8f438304d36bb7b7b0f28938910f9736c846b4a6
William Rodriguez <william@rodriguezfamily.tv>
2020-05-04 13:58:36 -0700

    Add UI elements

--- Dotify/app/src/main/java/com/example/dotify/MainActivity.kt
+++ Dotify/app/src/main/java/com/example/dotify/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private var numPlays = Random.nextInt(1000, 10000000)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvPlays = findViewById<TextView>(R.id.tvPlays)
+        val playText = "${numPlays.toString()}  plays"
+        tvPlays.text = playText
     }
 }
